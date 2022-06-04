cedb0abab5644ff7da9d9131f97b17d26ec4f085
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-04 12:23:23 -0300

    my commit

--- src/de_project/usd_brl.py
+++ src/de_project/usd_brl.py
@@ -25,11 +25,9 @@ def get_data_usd_brl():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(
-        ChromeDriverManager().install(),
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"),
         chrome_options=chrome_options,
     )
     driver.get(INVESTING_PAGE_URL)
