fe45eddf81a71fe3a71c00be9297b7e867060465
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-04 14:16:12 -0300

    This

--- src/de_project/get_table_pmi.py
+++ src/de_project/get_table_pmi.py
@@ -27,6 +27,7 @@ def get_data_pmi():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('window-size=1920x1080')
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(
         executable_path=os.environ.get("CHROMEDRIVER_PATH"),
--- src/de_project/usd_brl.py
+++ src/de_project/usd_brl.py
@@ -29,7 +29,8 @@ def get_data_usd_brl():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("window-size=1920x1080")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(
         ChromeDriverManager().install(),
         chrome_options=chrome_options,
