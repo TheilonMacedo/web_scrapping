ac160911eae8e5ce2a8b2910d3507906f4d35edc
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-04 12:35:40 -0300

    my commit

--- src/de_project/get_table_pmi.py
+++ src/de_project/get_table_pmi.py
@@ -23,8 +23,11 @@ def get_data_pmi():
     )
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(
         executable_path=os.environ.get("CHROMEDRIVER_PATH"),
         chrome_options=chrome_options,
--- src/de_project/usd_brl.py
+++ src/de_project/usd_brl.py
@@ -24,8 +24,11 @@ def get_data_usd_brl():
     INVESTING_PAGE_URL = "https://br.investing.com/currencies/usd-brl-historical-data"
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(
         executable_path=os.environ.get("CHROMEDRIVER_PATH"),
         chrome_options=chrome_options,
