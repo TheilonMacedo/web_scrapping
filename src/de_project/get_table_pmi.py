186a3b7cf71545b6fa80bedb3ff34168ba27232e
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-04 14:18:01 -0300

    This

--- src/de_project/get_table_pmi.py
+++ src/de_project/get_table_pmi.py
@@ -27,7 +27,9 @@ def get_data_pmi():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument('window-size=1920x1080')
+    chrome_options.add_argument("window-size=1920x1080")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(
         executable_path=os.environ.get("CHROMEDRIVER_PATH"),
--- src/de_project/usd_brl.py
+++ src/de_project/usd_brl.py
@@ -30,6 +30,8 @@ def get_data_usd_brl():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("window-size=1920x1080")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(
         ChromeDriverManager().install(),
