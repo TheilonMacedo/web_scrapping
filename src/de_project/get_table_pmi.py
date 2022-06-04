f40ffbf991ff314526011f943517bbeb8e8cc5f9
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-04 04:20:22 -0300

    This

--- src/de_project/get_table_pmi.py
+++ src/de_project/get_table_pmi.py
@@ -23,10 +23,12 @@ def get_data_pmi():
     )
 
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(
-        ChromeDriverManager().install(),
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"),
         chrome_options=chrome_options,
     )
     driver.get(INVESTING_PAGE_URL)
