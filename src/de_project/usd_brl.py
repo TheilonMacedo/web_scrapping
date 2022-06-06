b49e93e1b5c52559543ed47f17feed2e2c66a2cf
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-06 00:38:25 -0300

    This

--- src/de_project/usd_brl.py
+++ src/de_project/usd_brl.py
@@ -31,7 +31,7 @@ def get_data_usd_brl():
     chrome_options.add_argument("--disable-dev-shm-usage")
     # chrome_options.add_argument("window-size=1920x1080")
     # chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver = webdriver.Chrome(
         ChromeDriverManager().install(),
