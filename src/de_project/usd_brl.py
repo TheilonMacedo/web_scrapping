c2942289be9d2a9a55fd4669a2197d8847046fe1
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-06 20:07:34 -0300

    make it better

--- src/de_project/usd_brl.py
+++ src/de_project/usd_brl.py
@@ -31,7 +31,7 @@ def get_data_usd_brl():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("window-size=1920x1080")
+    # options.add_argument("window-size=1920x1080")
     options.add_argument("--disable-gpu")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument("start-maximized")
