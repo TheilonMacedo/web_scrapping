194468a4e455480881f61b51f5ece87af3c8a151
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-06 20:49:04 -0300

    make it better

--- src/de_project/get_table_pmi.py
+++ src/de_project/get_table_pmi.py
@@ -32,7 +32,7 @@ def get_data_pmi():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("window-size=1920x1080")
+    options.add_argument("window-size=1400x800")
     options.add_argument("--disable-gpu")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument("start-maximized")
