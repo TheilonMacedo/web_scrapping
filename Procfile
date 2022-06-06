e3b3439cdd6214364002f54eaa474706b3e329ab
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-06 00:31:25 -0300

    This

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run src/de_project/main.py
+web: streamlit run src/de_project/main.py
--- src/de_project/usd_brl.py
+++ src/de_project/usd_brl.py
@@ -29,11 +29,12 @@ def get_data_usd_brl():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("window-size=1920x1080")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--remote-debugging-port=9222")
+    # chrome_options.add_argument("window-size=1920x1080")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--remote-debugging-port=9222")
     # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(ChromeDriverManager().install(),
+    driver = webdriver.Chrome(
+        ChromeDriverManager().install(),
         chrome_options=chrome_options,
     )
     driver.get(INVESTING_PAGE_URL)
