9d2cc2f0b9bbc525c1710b752a0af13ef036c579
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-08 20:38:48 -0300

    make it better

--- src/de_project/main.py
+++ src/de_project/main.py
@@ -5,7 +5,8 @@ from send_msg import send
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job("cron", day_of_week="mon", hour=20)
+
+@sched.scheduled_job("cron", day_of_week="wed", hour=20, minute=43)
 def scheduled_job():
     send()
     print("This job is run monday at 20:00")
