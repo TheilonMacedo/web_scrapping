3775363c390549ebd6c57605535256a1ab51a410
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-08 21:07:30 -0300

    make it better

--- src/de_project/main.py
+++ src/de_project/main.py
@@ -5,10 +5,15 @@ from send_msg import send
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job("cron", day_of_week="wed", hour=21, minute=2)
-def scheduled_job():
-    send()
-    print("This job is run monday at 20:00")
+@sched.scheduled_job("interval", seconds=4)
+def timed_job():
+    print("This job is run every three minutes.")
+
+
+# @sched.scheduled_job("cron", day_of_week="wed", hour=20, minute=43)
+# def scheduled_job():
+#     send()
+#     print("This job is run monday at 20:00")
 
 
 sched.start()
