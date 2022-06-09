eda3934d5af5b82b31a2f2cbd0eba6d00ac5ce80
TheilonMacedo <theilonhenrique@gmail.com>
2022-06-08 21:13:24 -0300

    make it better

--- src/de_project/main.py
+++ src/de_project/main.py
@@ -1,19 +1,17 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from pytz import timezone
 
 from send_msg import send
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job("interval", seconds=4)
-def timed_job():
-    print("This job is run every three minutes.")
-
-
-# @sched.scheduled_job("cron", day_of_week="wed", hour=20, minute=43)
-# def scheduled_job():
-#     send()
-#     print("This job is run monday at 20:00")
+@sched.scheduled_job(
+    "cron", day_of_week="wed", hour=20, minute=43, timezone="America/Bahia"
+)
+def scheduled_job():
+    send()
+    print("This job is run monday at 20:00")
 
 
 sched.start()
